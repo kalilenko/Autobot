0b84ef0ed7da437ff3492892e9b74462dafe5a0d
kalilenko <kalilenko@gmail.com>
2013-03-04 21:29:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1985,7 +1985,7 @@
   };
 
   announceCurate = function(obj) {
-    return API.sendChat("/em: " + obj.user.username + " ทำการเพิ่มเพลงลง Playlist");
+    return API.sendChat("@" + obj.user.username + " ทำการเพิ่มเพลงลง Playlist");
   };
 
   handleUserJoin = function(user) {
